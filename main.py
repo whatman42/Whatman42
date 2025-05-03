3b92acd6d82d1be817c466225b649622d8dd419a
whatman42 <raidvoltus@gmail.com>
2025-05-03 09:06:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -372,8 +372,8 @@ def check_and_reset_model_if_needed(ticker: str, current_features: list[str]):
         logging.debug(f"{ticker}: Struktur fitur sama — model tidak di-reset")
         
 # Konstanta threshold (letakkan di atas fungsi analyze_stock)
-MIN_PRICE = 500
-MAX_PRICE = 2000
+MIN_PRICE = 10
+MAX_PRICE = 200000
 MIN_VOLUME = 10000
 MIN_VOLATILITY = 0.005
 MIN_PROB = 0.7
