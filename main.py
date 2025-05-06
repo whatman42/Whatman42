d532c8eab823a84b94966bf2a6d4b90b33407cbb
whatman42 <raidvoltus@gmail.com>
2025-05-06 19:54:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def calculate_indicators(df: pd.DataFrame) -> pd.DataFrame:
     df["Support"] = df["Low"].rolling(window=48).min()
     df["Resistance"] = df["High"].rolling(window=48).max()
 
-    df["ROC"] = momentum.ROCIndicator(df["Close"], window=12).roc()  # atau ubah window sesuai preferensi
+    df["ROC"] = momentum.ROCIndicator(df["Close"], window=5).roc()  # atau ubah window sesuai preferensi
     df["RSI"] = momentum.RSIIndicator(df["Close"], window=14).rsi()
     df["EMA_5"] = trend.EMAIndicator(df["Close"], window=5).ema_indicator()
     df["EMA_10"] = trend.EMAIndicator(df["Close"], window=10).ema_indicator()
@@ -180,6 +180,7 @@ def calculate_indicators(df: pd.DataFrame) -> pd.DataFrame:
     df["hour"] = df.index.hour
     df["is_opening_hour"] = (df["hour"] == 9).astype(int)
     df["is_closing_hour"] = (df["hour"] == 15).astype(int)
+    df["return_prev_day"] = df["Close"].pct_change(periods=HOURS_PER_DAY)
     df["daily_avg"] = df["Close"].rolling(HOURS_PER_DAY).mean()
     df["daily_std"] = df["Close"].rolling(HOURS_PER_DAY).std()
     df["daily_range"] = df["High"].rolling(HOURS_PER_DAY).max() - df["Low"].rolling(HOURS_PER_DAY).min()
